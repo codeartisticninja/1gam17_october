2847993f8c39243bb6cbd5f34330b80f7289a922
codeartisticninja <the@codeartistic.ninja>
2017-10-10 21:27:04 +0200

    Spider Aye :p

--- src/script/_classes/scenes/actors/Aye.ts
+++ src/script/_classes/scenes/actors/Aye.ts
@@ -19,8 +19,8 @@ export default class Aye extends Actor {
   constructor(scene:Scene, obj:any) {
     super(scene, obj);
     this.shape = "circle";
-    // this.setAnchor(this.size.x/2, this.size.y-15);
-    // this.size.set(60, 30);
+    this.setAnchor(this.size.x/2, this.size.y-15);
+    this.size.set(60, 30);
     this.addAnimation("idle",  [ 0, 1, 2, 3, 4, 5, 6, 7]);
     this.addAnimation("walk",  [ 8, 9,10,11,12,13,14,15], 0);
     this.addAnimation("do",    [16,17,18,19,20,21,22,23]);
@@ -43,7 +43,9 @@ export default class Aye extends Actor {
     }
     if (this.state) return super.update();
     if (joy.dir.magnitude) {
-      this.velocity.copyFrom(joy.dir).multiplyXY(8);
+      this.velocity.magnitude = joy.dir.x * 8;
+      this.velocity.angle = this.rotation + Math.PI/2*this.scale.x;
+      this.angularVelocity = joy.dir.y * .1;
       this.playAnimation("walk");
       this.animationFrame += joy.dir.magnitude;
       if (joy.dir.x < 0) {
@@ -54,6 +56,7 @@ export default class Aye extends Actor {
       }
     } else {
       this.velocity.set(0);
+      this.angularVelocity=0;
       this.playAnimation("idle");
     }
     super.update();
